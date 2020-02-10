4fb7366d6715ef270b8e600139b4bf2a9ada182b
Melaku <alebachewtegegne2017@yahoo.com>
2020-02-09 22:15:13 -0500

    I deleted the path I changed the name of the project

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="quiz_project"
+    version="0.0.1",
+    author="Alebachew Melaku",
+    author_email="alebachewtegegne2017@yahoo.com",
+    url="https://github.com/yourusername/yourproject",
+    description="it runns any python code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    extras_require=[],
+    tests_require=['pytest'],
+    python_requires='>=3.6',
+)
---
+++ tests/test_game_project.py
@@ -0,0 +1,4 @@
+import game_project
+
+def test_import():
+    assert game_project
